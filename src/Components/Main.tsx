7b0063b7ff79cb92520d0f68faac6bffe4be1948
Emil Kopytek <107545485+CeDwukropek@users.noreply.github.com>
2024-02-29 14:32:55 +0100

    switched to hook

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useTasks } from "../Hooks/useTasks";
+import { CreateFormData, useTasks } from "../Hooks/useTasks";
 import { Task } from "./Task";
 import {v4 as uuid} from 'uuid'
 import {useNavigate} from "react-router-dom";
@@ -12,55 +12,23 @@ import * as yup from "yup";
 import {yupResolver} from "@hookform/resolvers/yup";
 import {useForm} from "react-hook-form";
 
-interface CreateFormData {
-  description: string
-}
-
 export const Main = () => {
   const navigate = useNavigate()
   const auth = getAuth()
   const [user] = useAuthState(auth);
+  const schema = yup.object().shape({
+    description: yup.string().min(1).required("You must add a description")
+  });
+
+  const {register, handleSubmit, formState: {errors}} = useForm<CreateFormData>({
+    resolver: yupResolver(schema),
+  })
+
+  const {tasksList, addTask, removeTask, completeTask, getTasks} = useTasks()
 
   if(!user) {
     console.log("not logged")
     navigate('/')
-  }
-  const [tasksList, setTasksList] = useState<Task[] | null>(null)
-  const tasksRef = collection(db, "todos");
-  const schema = yup.object().shape({
-      description: yup.string().min(1).required("You must add a description")
-  });
-
-  const {register, handleSubmit, formState: {errors}} = useForm<CreateFormData>({
-      resolver: yupResolver(schema),
-  })
-
-  const onCreateTask = async (data: CreateFormData) => {
-      await addDoc(tasksRef, {
-          ...data,
-          completed: false,
-          userId: user?.uid,
-      })
-
-      let form = document.getElementsByTagName("form")[0]
-      form.reset()
-  }
-  
-  const getTasks = async () => {
-      const data = await getDocs(tasksRef)
-      setTasksList(
-          data.docs.map((doc) => ({...doc.data(), id: doc.id})) as Task[]
-      )
-  }
-
-  const removeTask = async (item: Task) => {
-    await deleteDoc(doc(db, "todos", item.id));
-  }
-
-  const completeTask = async (item: Task) => {
-    await updateDoc(doc(db, "todos", item.id),{
-        completed: !item.completed
-      })
   }
 
   useEffect(() => {
@@ -73,7 +41,7 @@ export const Main = () => {
     <Navbar/> 
     <div className="shadow"></div>
     <div className="bodyContainer">
-      <form className="taskForm" onSubmit={handleSubmit(onCreateTask)}>
+      <form className="taskForm" onSubmit={handleSubmit(addTask)}>
         <input type='text' {...register("description")} />
         <button type="submit" className="button">Create</button>
       </form>
